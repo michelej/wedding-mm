9b841759288920ff15957201454cc6d75e0470b3
Michel Escobar <michelejcs@hotmail.com>
2023-03-13 22:43:31 +0100

    added information and timeline

--- src/components/Information.vue
+++ src/components/Information.vue
@@ -8,9 +8,61 @@
                         <div class="card-content">
 
                             <header-couple></header-couple>
-                            <v-row justify="center">
-                                <v-col cols="12" xl="7">
-                                    <div class="info-text">
+                            <v-row justify="center mt-10">
+                                
+                                <v-col cols="12" xl="8" class="mt-10">
+                                    <v-timeline side="end" align="start">
+                                        <v-timeline-item dot-color="pink" size="small">
+                                            <div class="d-flex">
+                                                <strong class="me-4">17:30</strong>
+                                                <div>
+                                                    <strong>Ceremonia</strong>
+                                                    <div class="text-caption">
+                                                        Iglesia San Sebastián Mártir
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </v-timeline-item>
+
+                                        <v-timeline-item dot-color="teal-lighten-3" size="small">
+                                            <div class="d-flex">
+                                                <strong class="me-4">19:30 - 20:30</strong>
+                                                <div>
+                                                    <strong>Recepcion / Coctel</strong>
+                                                    <div class="text-caption mb-2">
+                                                        La Quinta de Jarama 
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </v-timeline-item>
+
+                                        <v-timeline-item dot-color="pink" size="small">
+                                            <div class="d-flex">
+                                                <strong class="me-4">20:30-23:30</strong>
+                                                <div>
+                                                    <strong>Cena</strong>
+                                                    <div class="text-caption mb-2">
+                                                        La Quinta de Jarama 
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </v-timeline-item>
+
+                                        <v-timeline-item dot-color="teal-lighten-3" size="small">
+                                            <div class="d-flex">
+                                                <strong class="me-4">23:00</strong>
+                                                <div>
+                                                    <strong>Musica & Open Bar</strong>    
+                                                    <div class="text-caption mb-2">
+                                                        La Quinta de Jarama 
+                                                    </div>                                                
+                                                </div>
+                                            </div>
+                                        </v-timeline-item>
+                                    </v-timeline>
+                                </v-col>
+                                <v-col cols="12" xl="4">
+                                    <div class="info-text texts">
                                         <p><strong>Recordad</strong> lo más importante es que vengáis con ilusión, alegría y
                                             ganas de
                                             pasarla bien. Pero si queréis hacernos un regalo, aqui tenéis nuestro número de
@@ -19,6 +71,7 @@
                                         <p>ES07 1465 0100 95 2043889715<br>Michel Escobar</p>
                                     </div>
                                 </v-col>
+
                             </v-row>
                         </div>
                     </v-card>
